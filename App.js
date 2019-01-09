89e8a798cb3eecd55cd4bd555bf468d20c00686f
raq <mirkanbaba1@gmail.com>
2019-01-09 03:25:16 -0600

    rotate works properly, when click somewhere creates a black dot

--- App.js
+++ App.js
@@ -10,13 +10,13 @@ import {Platform, StyleSheet, Text, View, Animated, TouchableOpacity, Dimensions
 export default class App extends Component {
   constructor(props) {
     super(props);
-  
+
+    this.displayData = [];
+
     this.state = {
       fadeValue: new Animated.Value(0),
       width: new Animated.Value(100),
-      rotateX: 0,
-      rotateY: 0,
-      oldValue: 0,
+      oldValue: [1, 1],
     };
     this.timer = null;
     this.stopFunction = this.stopFunction.bind(this);
@@ -64,41 +64,49 @@ export default class App extends Component {
     let locY = evt.nativeEvent.locationY;
     let endpoint = Math.atan(locY / locX)
     console.log(`locX: ${locX}, locY: ${locY}`)*/
-    let yukseklik = Dimensions.get('window').height;
+    let FRONT =  [Dimensions.get('window').height / 2, Dimensions.get('window').width / 2];
+    console.log(this.displayData) // 187.5, 333.5
+    let X = 400;
+    let Y = 200;
     let Yprime = evt.nativeEvent.locationX;
     let Xprime = evt.nativeEvent.locationY;
+    let result1 = (FRONT[1] - Yprime) / (FRONT[0] - Xprime);
+    let result = Xprime < FRONT[1] ? -(Math.atan(result1)) * 180 / 3.14 + 180 : -(Math.atan(result1)) * 180 / 3.14;
 
-    /*Animated.timing(this.state.rotate, {
-      toValue: locX/locY,
-      duration: 3000,
-      easing: Easing.linear
-    }).start();*/
+    this.setState({ anan: result, oldValue: [Xprime, Yprime] });
+    
+    this.displayData.push(<View style={{backgroundColor: "black", width:10, height:10, position: "absolute", marginTop: Xprime, marginLeft: Yprime}}></View>);
+    this.setState({
+      showdata : this.displayData
+    });
 
-    this.setState({ rotateX: Yprime/yukseklik * 180, oldValue: evt.nativeEvent.locationX + ", " + evt.nativeEvent.locationY })
-    //this.timer = setTimeout(this.changeDirection(evt), 200);
+    //var view = React.createElement(View, [{style: {backgroundColor:"blue", width: 100, height: 100}}], 'hello');
   }
 
   render() {
     // TODO: find a way to rotate the shape while pressing
     // TODO2: ilk baktigi yerin noktasi'ni ikinci tikladigi noktanin degerinden cikar, 
-    // bu sana karsi duvari vercek. karsi/hipotenus yap,  
+    // bu sana karsi duvari vercek. karsi/hipotenus yap
+    // center is around (200,400)
+    // WATAFAAAAK
     return (
-      <TouchableOpacity style={styles.container} onPressIn={(evt) => this.changeDirection(evt)} onPressOut={(evt) => this.changeDirection(evt)}>
-        {/*<Animated.View style={[styles.animationView,
-          { opacity: this.state.fadeValue }
-          ]}>
-        </Animated.View>*/}
+      <TouchableOpacity style={styles.container} onPress={(evt) => this.changeDirection(evt)}>
 
+        <View>
+          {this.displayData}
+        </View>
+      
         <Animated.View style={[styles.animationView,
           { width: this.state.width },
-          { transform:[ { rotateZ: (-this.state.rotateX) + 'deg' }] }
+          { transform:[ { rotateZ: (this.state.anan) + 'deg' }] }
           ]}>
         </Animated.View>
 
-        <TouchableOpacity onPress={this._increaseWidth}>
-          <Text style={styles.welcome}>{this.state.oldValue}</Text>
-        </TouchableOpacity>
-
+        {/*
+        <View onPress={this._increaseWidth}>
+          <Text style={styles.welcome}>{this.state.oldValue[0]}-{this.state.oldValue[1]}</Text>
+        </View>
+        */}
 
       </TouchableOpacity>
     );
@@ -115,6 +123,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    marginTop: 150,
   },
   instructions: {
     textAlign: 'center',
@@ -122,16 +131,16 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   animationView: {
-    backgroundColor: 'transparent',
     borderStyle: 'solid',
     borderLeftWidth: 50,
     borderRightWidth: 50,
-    borderBottomWidth: 100,
-    borderTopWidth: 100,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'transparent',
+    borderBottomWidth: 50,
+    borderTopWidth: 50,
     borderTopColor: 'red',
+    alignSelf: 'center',
+    position: "absolute",
+    marginTop: Dimensions.get('window').height / 2 - 100,
+    marginLeft: Dimensions.get('window').width / 2,
   },
   button: {
     backgroundColor: 'steelblue',
